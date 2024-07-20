f5b8e556ee50ad8b6ba272efe3655db48b5bd651
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-07-20 22:24:52 +0530

    First version of implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+
+let globalID = 0;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
 
+  function createTodo(event) {
+    event.preventDefault();
+    setTodos((oldTodo) => {
+      setTask("");
+      return [...oldTodo, { todo: task, id: globalID++ }];
+    });
+  }
+  function deleteItem(itemID) {
+    setTodos((oldTodos) => oldTodos.filter((item) => item.id !== itemID));
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Simple To Do App</h1>
+      <form onSubmit={createTodo}>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => {
+            setTask(e.target.value);
+          }}
+        />
+      </form>
+      <button type="submit" onClick={createTodo}>
+        Add Todo
+      </button>
+      <ul>
+        {todos.map((item, index) => {
+          return (
+            <div key={item.id}>
+              <li>{item.todo}</li>
+              <button onClick={() => deleteItem(item.id)}>Delete</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
-
-export default App
+export default App;
